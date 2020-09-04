60ab94bae36a3249300cadef08f24ad5db59aebf
Lương Minh Đức <57982097+minhduc19669@users.noreply.github.com>
2020-09-04 23:35:07 +0700

    wrong

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
 
-  <?php
-     function theArray($n,$a){
-		$b=[];
-		for($i=0;$i < ( $n - 1);$i++){
-		if($i==0){
-			$b[0]= 0 + $a[0] + $a[1];
-		}else{
-			$b[$i]=$a[$i - 1] + $a[$i] + $a[$i + 1];
-		}
-	}
-		return $b;
-}
+      function theArray($n, $a) {
+        $count = a.length;
+         $b = [];
+        for ($i = 0; $i <= $n - 1; $i++) {
+          if ($i == 0) {
+            $b[$i] = 0 + $a[0] + $a[1];
+          } else if ($i == $count - 1) {
+             $b[$i] = a[$i - 1] + a[$i] + 0;
+          } else {
+            $b[$i] = $a[$i - 1] + $a[$i] + $a[$i + 1];
+          }
+        }
+        return $b;
+      }
 var_dump(theArray(5,[4,0,1,-2,3]));
